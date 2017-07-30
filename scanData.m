95e2d842d91f5bb162460ece45601eec2d7a5687
evilying <evilying2016@gmail.com>
2017-07-30 13:59:19 -0400

    scan the data in 2121kids.csv into the main memory.

---
+++ scanData.m
@@ -0,0 +1,38 @@
+clear; 
+
+dataDir = '../'; 
+
+namefile = '2121kids.csv'; 
+
+% open file 
+fileId = fopen(fullfile(dataDir, namefile), 'r'); 
+
+% read first line 
+% fgetl(fileId); 
+% line = fgetl(fileId); 
+
+% parameters
+nrow = 2121; 
+nCols = 12949; 
+
+row = textscan(fileId, repmat('%s', 1, nCols), 'delimiter', ',', 'CollectOutput', true); 
+
+%% start to processing the data 
+parfor (i = 1: nrow, 4)
+    disp(['processing: ' num2str(i) 'th row']); 
+    data(i, :) = str2double(row{1}(i, :)); 
+end
+
+fclose(fileId);
+% matcase = zeros(nrow, nCols); 
+% irow = 1; 
+% 
+% while (~feof(fileId))
+% 
+%     irow = irow + 1; 
+%     
+%     line = fgetl(fileId); 
+%     matcase(irow, :) = sscanf(line, '%f'); 
+% end
+
+% xlswrite(fullfile(dataDir, 'clean.xlsx'), matcase); 
