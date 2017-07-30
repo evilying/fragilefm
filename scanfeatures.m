ffcd62ffa49d7c5ef7a75b393be9b89cff15f94d
evilying <evilying2016@gmail.com>
2017-07-30 14:10:38 -0400

    save the deserted features as well as remaining features

---
+++ scanfeatures.m
@@ -0,0 +1,37 @@
+dataDir = '../'; 
+
+namefile = 'featuresName.csv'; 
+
+% open file 
+fileId = fopen(fullfile(dataDir, namefile), 'r'); 
+line = fgetl(fileId); 
+
+nCols = 12949; 
+featList = textscan(line, repmat('%s', 1, nCols), 'Delimiter', ','); 
+
+featIndice = sort([nanColloc, missColloc]); 
+
+% feats(:) = featList{1, featIndice}; 
+
+feats = cell(1, size(featIndice, 2)); 
+
+for i = 1: length(feats)
+    
+    feats{i} = char(featList{1, featIndice(i)}); 
+end
+
+xlswrite('desertedfeatures.xlsx', feats(:)); 
+
+allIndice = 1: 12949; 
+importfeatIndice = sort(setdiff(allIndice, featIndice)); 
+
+importFeats = cell(1, size(importfeatIndice, 2)); 
+
+for i = 1: length(importFeats)
+    
+    importFeats{i} = char(featList{1, importfeatIndice(i)}); 
+end
+
+xlswrite('importfeatures.xlsx', importFeats(:)); 
+
+fclose(fileId); 
